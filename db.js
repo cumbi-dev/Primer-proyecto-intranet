327f4f223a86a8d3879a932b453b079f2bfe9e55
Juan Carlos Cumbicos <juan.carlos.cg.dev@gmail.com>
2026-01-02 14:19:32 +0100

    vuelta a local host

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql2/promise');
 
-// Creamos la conexión usando "Variables de Entorno"
-// Si existen las llaves del servidor, las usa. Si no, usa las de tu PC (localhost).
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '1234',
-  database: process.env.MYSQLDATABASE || 'intranet_db',
-  port: process.env.MYSQLPORT || 3306
+    host: 'localhost',
+    user: 'root',
+    // 🔑 OPCIÓN A: Si en Workbench entras SIN contraseña, deja esto así: ''
+    // 🔑 OPCIÓN B: Si usas contraseña, escríbela aquí: 'tu_password'
+    password: '1234', 
+    database: 'intranet_db', 
+    port: 3306 
 });
 
 module.exports = pool;
