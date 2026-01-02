7e48284648c98759007a2ee5da848fea6ae4ed22
Juan Carlos Cumbicos <juan.carlos.cg.dev@gmail.com>
2026-01-02 13:12:40 +0100

    Solución de rutas y puerto dinámico

--- db.js
+++ db.js
@@ -1,23 +1,13 @@
-require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-// Creamos la "piscina" con los datos de tu servidor local
+// Creamos la conexión usando "Variables de Entorno"
+// Si existen las llaves del servidor, las usa. Si no, usa las de tu PC (localhost).
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '1234',
+  database: process.env.MYSQLDATABASE || 'intranet_db',
+  port: process.env.MYSQLPORT || 3306
 });
 
-// Exportamos la promesa para poder usarla en otros archivos de forma moderna
-module.exports = pool.promise();
-
-// Prueba rápida de conexión
-//pool.getConnection((err, connection) => {
- // if (err) {
-//    console.error("❌ Error al conectar a la base de datos:", err.message);
- // } else {
- //   console.log("✅ ¡Conexión exitosa a MySQL!");
-  //  connection.release(); // "Soltamos" la conexión de vuelta a la piscina
- // }
-// });
+module.exports = pool;
